de7187647d0c375aa92eb2c219d215274abf2a66
harsh <harshpal.0890@gmail.com>
2025-06-20 00:54:03 +0530

    Added folder structure

---
+++ backend/src/main/java/com/nemo/vercel/VercelApplication.java
@@ -0,0 +1,13 @@
+package com.nemo.vercel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VercelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VercelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nemo/vercel/VercelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemo.vercel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VercelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
